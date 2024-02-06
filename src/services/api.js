e9cdabdd90350e231cf4f55975eb9e65a71fdd16
Walter Souza <158614177+WaltherSouza@users.noreply.github.com>
2024-02-06 14:50:50 -0300

    projeto finalizado com REACT JS

---
+++ src/services/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: "https://viacep.com.br/ws/01310930/json/"
+});
+
+
+export default api;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
